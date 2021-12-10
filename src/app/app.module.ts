a861ca33dff69b7d8b2186eeb3314904212ee91f
ruben jallifier-talmat <ruben.jallifier@gmail.com>
2021-12-10 12:21:44 +0100

    exercice 10 fonctionnel (je reviens sur  l'ex 8 :P )

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,16 @@ import { AppComponent } from './app.component';
 import { DisplayStatComponent } from './display-stat/display-stat.component';
 import { StatListComponent } from './stat-list/stat-list.component';
 import { CreateStatComponent } from './create-stat/create-stat.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent, DisplayStatComponent, StatListComponent, CreateStatComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    DisplayStatComponent,
+    StatListComponent,
+    CreateStatComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/create-stat/create-stat.component.ts
+++ src/app/create-stat/create-stat.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { StatistiqueService } from '../statistique.service';
+import { Statistique } from '../models/statistique';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-stat',
   templateUrl: './create-stat.component.html',
-  styleUrls: ['./create-stat.component.scss']
+  styleUrls: ['./create-stat.component.scss'],
 })
 export class CreateStatComponent implements OnInit {
+  public title!: string;
+  public id!: string;
+  public value!: number;
+  constructor(public statService: StatistiqueService, public router: Router) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  submit(form: NgForm) {
+    this.addStat();
+    this.router.navigate(['/stat-list']);
   }
 
+  addStat() {
+    let newStat = new Statistique(this.title, this.id, this.value);
+    this.statService.tabStat.push(newStat);
+  }
+  ngOnInit(): void {}
 }
